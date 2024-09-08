22962ed72595a24dddffad67bbb1ac0b1403d2f8
medinanzz <apololick2@gmail.com>
2024-09-08 18:22:50 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var botao = document.querySelector('#buttom');
+    if (botao) {
+        botao.addEventListener('click', function() {
+            alert('Veja mais em templo do reverendo!');
+        });
+    }
+});
+// document.getElementById("buttom").addEventListener("click", function() {
+//     alert("veja mais em templo do reverendo!");
+// });
